db5ed170b5e11d7ff3c3ac3b8f481e54d438c9e2
NicolaBoniello <114167768+NicolaBoniello@users.noreply.github.com>
2023-04-15 12:33:01 +0200

    Add files via upload

---
+++ demo/src/main/java/sitowebmine/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package sitowebmine.demo.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sitowebmine.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
